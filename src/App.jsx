17939363693234666f4a60176038c1d747031b8d
Vallam Gurubabu <vallamgurubabu@gmail.com>
2025-10-04 19:05:10 +0530

    added logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -38,6 +37,7 @@ function App() {
       </div>
       <Footer />
     </Router>
+     
   );
 }
 
